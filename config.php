d4411c219a4f20d8b32339f75299b7053232793f
Chris Pynegar <chrispynegar2157@gmail.com>
2012-05-17 19:55:55 +0100

    Configuration file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array();
+
+$config['db_type'] = 'mysql';
+$config['db_server'] = 'localhost';
+$config['db_username'] = 'contentexpress';
+$config['db_password'] = 'expresspass';
+$config['db_name'] = 'contentexpress';
+$config['db_tbl_prefix'] = 'tbl_';
+$config['site_root'] = '/Users/chrispynegar2157/Sites/applications/contentexpress/';
+$config['base_url'] = 'http://localhost/~chrispynegar2157/applications/contentexpress/';
+
+?>
