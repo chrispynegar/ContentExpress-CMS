91fd73e6b0020377b5458420b9a7288fbdb5cfb7
Chris Pynegar <chrispynegar2157@gmail.com>
2012-06-19 21:43:25 +0100

    Page component

--- components/com_page/menu-output.php
+++ components/com_page/menu-output.php
@@ -1,8 +1,20 @@
-<?php 	?>
+<?php 
+
+$options = explode(', ', $item->options);
+
+$option = array();
+foreach($options as $key => $value) {
+	$option_value = explode(': ', $value);
+	$option[strtolower($option_value[0])] = $option_value[1];
+}
+
+$com_page = Page::find_by_id($option['page']);
+
+?>
 
 <div class="page-alias-here">
-	<h1 class="title">Page Title Here</h1>
+	<h2 class="title"><?php echo $com_page->title; ?></h2>
 	<div class="content">
-		Page Content Here
+		<?php echo nl2br($com_page->content, ALLOWED_TAGS); ?>
 	</div>
 </div>
--- config.php
+++ config.php
@@ -17,5 +17,6 @@ $config['db_name'] = 'contentexpress';
 $config['db_tbl_prefix'] = 'tbl_';
 $config['site_root'] = '/Users/chrispynegar2157/Sites/applications/contentexpress/';
 $config['base_url'] = 'http://localhost/~chrispynegar2157/applications/contentexpress/';
+$config['allowed_tags'] = '<p><a><b><i><strong><em><br><ul><li><ol><h1><h2><h3><h4><h5><h6><blockquote><cite><code><strike><acronym><abbr>';
 
 ?>
